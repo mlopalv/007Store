bd1f8f9be107346699dc8e8a01b0d67723b1a251
chevogomez-git <chevogomez-git@users.noreply.github.com>
2023-07-04 10:58:09 -0600

    cambios a app, styles y index

--- app.js
+++ app.js
@@ -11,16 +11,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/index.html"));
 });
 
-app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/register.html"));
-});
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/login.html"));
-});
-
 app.get('/product', (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/productDetails.html"));
+});
+
+app.get('/carrito', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/productCart.html"));
 });
 
 app.listen(3000, () => {
