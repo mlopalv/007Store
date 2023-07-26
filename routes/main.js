2bf16f33d1dd2d720dad26f46d204436dfae6ab6
chevogomez-git <chevogomez-git@users.noreply.github.com>
2023-07-26 17:24:15 -0600

    cambios en app.js y main.js

--- routes/main.js
+++ routes/main.js
@@ -6,4 +6,16 @@ router.get('/', (req, res) => {
     res.render('../views/index');
 });
 
+router.get('/carrito', (req, res) => {
+    res.render('productCart');
+});
+
+router.get('/registro', (req, res) => {
+    res.render('register');
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
 module.exports = router;
