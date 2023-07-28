b3fcdfaf3370f81386ba3b3d85bdc0edb0da6ad0
mlopalv <mlopalv@gmail.com>
2023-07-28 05:49:27 -0500

    adicionando cambios app.js de sprint 4

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
-
 const path = require("path");
+/* Importamos method-override para poder usar acciones PUT y DELETE desde los formularios HTML */
+const methodOverride = require("method-override");
 
 const publicPath = path.resolve(__dirname, "./public");
 
 app.use(express.static(publicPath));
-
+//Configuracion para procesamiento de envios POST
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+//Uso de method-override dentro de esta aplicacion
+app.use(methodOverride("_method"));
+//Indicamos que el template-engine a usar es ejs
 app.set('view engine','ejs');
 
 //Requerir las rutas
