22de170615453d1f4216e2f466dd5b7e23f6fdf9
chevogomez-git <chevogomez-git@users.noreply.github.com>
2023-08-10 15:53:05 -0600

    agregando registro de usuarios

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -5,9 +5,108 @@ const path = require('path');
 //const { encode } = require('punycode');
 
 const usersController = {
+    //seccion para registrar nuevos usuarios - mostrar la vista
+    register: function(req,res) {
+        return res.render('register');
+    },
+    //seccion para registrar nuevos usuarios - procesar el registro cuando dan click a REGISTRAR
+    processRegister: function(req,res) {
+        let errors = validationResult(req);
+
+        console.log(JSON.stringify(errors,null,4) );
+ 
+        let usuarioARegistrar = {
+            id:0,
+            nombre: req.body.nombre,
+            apellido: req.body.apellido,
+            usuario: req.body.usuario,
+            email: req.body.email,
+            nacimiento: req.body.nacimiento,
+            domicilio: req.body.domicilio,
+            pais: req.body.pais,
+            password1: req.body.pass1,
+            password2: req.body.pass2,            
+            categoria: "general"
+        };
+
+        console.log('To String = ' + JSON.stringify(req.body.pais));
+
+        if (errors.isEmpty()){
+            console.log('NO hay errores');
+            //console.log(JSON.stringify(errors,null,4));
+
+            let usersJSON = fs.readFileSync(path.resolve(__dirname, '../database/usuarios.json'));
+            let users;
+            if(usersJSON==""){
+                users=[]
+            } else {
+                users = JSON.parse(usersJSON);
+            }
+
+
+
+            let arrayOfIds = [];
+            let usuarioExistente = false;
+
+            for (let i=0; i < users.length; i++){
+                console.log('Looping en el for');
+                arrayOfIds.push(users[i].id);
+                if(users[i].email==req.body.email){
+                    /*
+                    if(bcrypt.compareSync(req.body.password,users[i].password)){
+
+                    }
+                    */
+                    usuarioExistente =true;
+                    break;
+                }
+            }
+
+          
+            if(usuarioExistente == true){
+                console.log('Usuario ya existe en bd');
+                return res.render('register', {errors:[
+                    {msg: 'Este correo ya esta registrado'}
+                ], oldData: usuarioARegistrar})
+            } else {
+                console.log('Usuario inexistente');
+            }
+
+            let maxID = Math.max(...arrayOfIds);
+            //let maxID = Math.max(users.id);
+
+            console.log('Maximo ID de usuarios = ' + maxID);
+    
+            usuarioARegistrar.id = maxID + 1;
+
+            users.push(usuarioARegistrar);
+            users = JSON.stringify(users,null,4);
+            //No estoy seguro si ya desde aqui hay que asignarle sesion al nuevo usuario
+            //req.session.usuarioLogueado = usuarioARegistrar;
+            
+
+            usuarioARegistrar = JSON.stringify(usuarioARegistrar,null,4);
+
+            console.log('Nuevo usuario = ' + usuarioARegistrar);
+            
+            fs.writeFileSync(path.resolve(__dirname, '../database/usuarios.json'),users);
+
+            res.render('profile');
+
+
+        } else {
+            console.log('Hay errores al registrar nuevo usuario');
+            console.log(JSON.stringify(errors,null,4));
+            console.log(JSON.stringify(usuarioARegistrar,null,4));
+            return res.render('register', {errors: errors.errors, oldData: usuarioARegistrar});
+        }
+    },    
+
+    //seccion para hacer login - mostrar la vista
     login: function(req,res) {
         return res.render('login');
     },
+    //seccion para hacer login - cuando el usuario da click en LOGIN
     processLogin: function(req,res) {
         let errors = validationResult(req);
         
@@ -40,6 +139,8 @@ const usersController = {
             }
 
             req.session.usuarioLogueado = usuarioALoguearse;
+
+
 
             res.redirect('/users/profile');
 
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,42 +2,33 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-//const multer = require("multer");
 const { check, validationResult, body } = require('express-validator');
-
-//const currentPath = path.resolve(__dirname,'../controllers/usersController.js');
-
-//console.log('Current path = ' + currentPath);
-
-//const usersController = require(currentPath);
 
 const usersController = require('../controllers/usersController');
 
-//Uso de multer para almacenamiento de imagenes 
-/*
-const multerDiskStorage = multer.diskStorage(
-    //Objeto literal con dos funciones
-    {
-        destination: function (req, file, cb) {
-            cb(null, path.resolve(__dirname,"../public/images/productos"));
-        },
-        filename: function (req, file, cb) {
-            cb(null, Date.now() + path.extname(file.originalname));
-        }
-    }
-
-);
-*/
-
-//console.log('llegue hasta aqui');
-//Variable que sirve como middleware para la carga con multer
-//const uploadFile = multer({ storage: multerDiskStorage });
-
+const validaciones = [
+    body('nombre').notEmpty().withMessage('El nombre no puede estar vacio'),
+    body('apellido').notEmpty().withMessage('El apellido no puede estar vacio'),
+    body('usuario').notEmpty().withMessage('El nombre de usuario no puede estar vacio'),
+    body('email').isEmail().withMessage('Email invalido'),
+    body('nacimiento').notEmpty().withMessage('No hay fecha de cacimiento seleccionada'),
+    //body('nacimiento').isDate().withMessage('Fecha equivocada'),
+    body('domicilio').notEmpty().withMessage('La direccion no puede estar vacia'),  
+    body('pais').notEmpty().withMessage('No hay pais seleccionado'),      
+    body('pais').not().isIn(['Seleccionar pais','']).withMessage('No hay pais seleccionado'),
+    
+    body('pass1').notEmpty().withMessage('Contrasena 1 vacia'),
+    body('pass2').notEmpty().withMessage('Contrasena 2 vacia')
+];
 
 /** Chevo **/
 router.get('/users/login', usersController.login);
 
 router.post('/users/login', usersController.processLogin);
+
+router.get('/users/register', usersController.register);
+
+router.post('/users/processRegister', validaciones, usersController.processRegister);
 
 //router.get('/users/profile', usersController.showProfile);
 /*
