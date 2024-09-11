21e25dca20e423327a0c280e22cf342930e84518
Mauricio López <130235835+mlopalv@users.noreply.github.com>
2024-09-11 15:38:23 -0500

    Update config.js

--- database/config/config.js
+++ database/config/config.js
@@ -4,20 +4,23 @@ module.exports ={
     "password": "AVNS_ka_osaDp0lyXrd2mcYs",
     "database": "defaultdb",
     "host": "mysql-8f6419b-global007store.c.aivencloud.com",
+    "port":"14974",
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+   "username": "avnadmin",
+    "password": "AVNS_ka_osaDp0lyXrd2mcYs",
+    "database": "defaultdb",
+    "host": "mysql-8f6419b-global007store.c.aivencloud.com",
+    "port":"14974",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "avnadmin",
+    "password": "AVNS_ka_osaDp0lyXrd2mcYs",
+    "database": "defaultdb",
+    "host": "mysql-8f6419b-global007store.c.aivencloud.com",
+    "port":"14974",
     "dialect": "mysql"
   }
 }
